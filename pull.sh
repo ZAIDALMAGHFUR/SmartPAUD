be7e97cd0edb7c155776fe5434e92e8e56907d2a
ZAIDALMAGHFUR <almaghfurzaid@gmail.com>
2024-08-31 11:57:30 +0700

    token

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ganti `your_token_here` dengan token akses personal GitHub Anda
-GITHUB_TOKEN="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"
+GITHUB_TOKEN=""
 
 echo "Pulling latest changes from the repository..."
 git pull https://ZAIDALMAGHFUR:$GITHUB_TOKEN@github.com/ZAIDALMAGHFUR/SmartPAUD.git
